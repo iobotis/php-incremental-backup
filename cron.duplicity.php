5f9d685e03312ba5a4c29defcb241d8bf7a59b44
iobotis <iobotis4@gmail.com>
2016-12-27 17:59:12 +0200

    Directory structure updated according to psr-4.

--- cron.duplicity.php
+++ cron.duplicity.php
@@ -6,26 +6,31 @@
  * @since 23/9/2016
  */
 
-require_once( 'IncrementalBackup.php' );
+require_once('settings.php');
+
+require_once('src/Backup/Duplicity.php');
+require_once('src/Backup/IncrementalBackup.php');
+
+use Backup\IncrementalBackup;
+use Backup\Duplicity;
 
 echo "Version: " . Duplicity::getVersion() . "\n";
 
-$backup = new Duplicity( '/path/to/backup', '/path/to/save' );
+$backup = new Duplicity( $path_to_backup, $path_to_save );
 
 //$backup->setPassPhrase( 'abcdef' );
 
-$backupClass = new IncrementalBackup ( $backup );
+$backupClass = new IncrementalBackup ($backup);
 
 $backups = $backupClass->getAllBackups();
 foreach ($backups as $time) {
     echo 'There is a backup at ' . $time . "\n";
 }
 
-if( $backupClass->isChanged() ) {
+if ($backupClass->isChanged()) {
     // back me up.
     echo 'Back up initiated' . "\n";
     $backupClass->createBackup();
-}
-else {
+} else {
     echo 'No need to backup.' . "\n";
 }
