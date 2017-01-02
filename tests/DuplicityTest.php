e00a5fc200fca8ed645963d3d947974b03577b5d
iobotis <iobotis4@gmail.com>
2017-01-03 00:56:38 +0200

    Duplicity class unit test added.

---
+++ tests/DuplicityTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jb
+ * Date: 1/2/17
+ * Time: 9:58 PM
+ */
+
+namespace Backup\tests;
+
+use Backup\Duplicity;
+
+class DuplicityTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Duplicity
+     */
+    protected $duplicity;
+
+    public function setUp()
+    {
+        $this->duplicity = new Duplicity(__DIR__, __DIR__);
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        $this->duplicity = null;
+        parent::tearDown();
+    }
+
+    public function testIsInstalled()
+    {
+        $this->duplicity->isInstalled();
+    }
+}
+
+function exec($command, $output, $exitCode)
+{
+    echo 'worked';
+}
